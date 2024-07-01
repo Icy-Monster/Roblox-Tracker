ca63814ffe47e55366c992b8d54d7a36d5ece9cb
IcyMonstrosity <102822122+Icy-Monster@users.noreply.github.com>
2024-07-01 23:25:31 +0200

    Display improvements
    Only currently playing users are now displayed, people who stopped playing are removed. Also no duplicates of messages

--- Discord.py
+++ Discord.py
@@ -1,10 +1,11 @@
 import requests
 import json
 
-import asyncio 
+import asyncio
 import datetime
 
 import discord
+
 
 
 Token = "" # Set this to your Discord Bot's Token
@@ -14,8 +15,10 @@ Client = discord.Client(intents=Intents)
 ##########################################
 
 # These are .txt files that list the user ids, you can find out how to get them here: https://github.com/Icy-Monster/Roblox-Group-Scraper for other groups
-AdminList = open(r"/home/icy/Desktop/Admins.txt", 'r').readlines()
-StarList = open(r"/home/icy/Desktop/StarCreators.txt", 'r').readlines()
+AdminList = open(r"C:\Users\Monster\Desktop\Admins.txt", 'r').readlines() # change the location to fit your file path
+StarList = open(r"C:\Users\Monster\Desktop\StarCreators.txt", 'r').readlines() # change the location to fit your file path
+
+Messages = {}
 
 # The UsernameRequest is not in order, so this is just a quick botch
 async def GetUser(UserList: tuple[str], ID: int):
@@ -27,7 +30,7 @@ async def FetchStatus(UserIDs: tuple[int], Channel: discord.channel, Emoji: str)
     headers = {
         'Content-Type': 'application/json','Accept': 'application/json',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0',
-        'Cookie': ".ROBLOSECURITY=_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|" # Set this to your Roblox Cookie, I recommend making a new account for this- as to not ratelimit your main account
+        "Cookie": ".ROBLOSECURITY=_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|" # Add your Roblox Security cookie here, I recommend using a spare account so you don't get timed out
     }
 
     StatusRequest = requests.post('https://presence.roblox.com/v1/presence/users', headers=headers, data=json.dumps({"userIds": UserIDs}))
@@ -37,30 +40,30 @@ async def FetchStatus(UserIDs: tuple[int], Channel: discord.channel, Emoji: str)
 
     for User in json.loads(StatusRequest.content.decode('utf-8'))["userPresences"]:
         Playing = User["userPresenceType"] == 2
-        
-        if not Playing: pass
-
-        content = ""
-        color = 0xff0000
-        Description = "**Cannot join user**"
-
-        if User["placeId"] != None:
-            content = "@everyone"
-            color = 0x00af00
-            Description = "**[Join](https://www.roblox.com/games/start?placeId=16302670534&launchData=" + str(User["placeId"]) + "/" + User["gameId"] + ") them in ["+ User["lastLocation"] +"](https://www.roblox.com/games/"+ str(User["placeId"]) +")**"
-
         UserInfo = await GetUser(UsernameRequest, User["userId"])
+
+        if not Playing:
+            if UserInfo["name"] in Messages:
+                await Messages[UserInfo["name"]].delete()
+                del Messages[UserInfo["name"]]
+            continue
+        if User["placeId"] == None: continue
+        if UserInfo["name"] in Messages: continue
+
+        Description = "**[Join](https://www.roblox.com/games/start?placeId=16302670534&launchData=" + str(User["placeId"]) + "/" + User["gameId"] + ") them in ["+ User["lastLocation"] +"](https://www.roblox.com/games/"+ str(User["placeId"]) +")**"
 
         Title = "** "+ Emoji +" "+ UserInfo["displayName"] +" (@"+ UserInfo["name"] +")**"
 
         Embed = discord.Embed(
             title=Title,
             description=Description,
-            color=color,
+            color=0x00af00,
             timestamp=datetime.datetime.now(),
             url="https://www.roblox.com/users/"+ str(User["userId"]) +"/profile",
             )
-        await Channel.send(embed=Embed,content=content)
+        
+        Message = await Channel.send(embed=Embed,content= Emoji + "@everyone")
+        Messages[UserInfo["name"]] = Message
 
 
 
@@ -70,11 +73,14 @@ async def on_ready():
 
     # 50 users are sent at a time, for faster logging- 50 is the limit Roblox allows
     while True:
-        for i in range( round(sum(1 for _ in AdminList) / 50) ):
-            UserList = AdminList[i*50  :  i*50 + 50]
-
-            await FetchStatus(UserList, Channel, "🛡️ ") # ⭐  🛡️
-            await asyncio.sleep(18)
+        try:
+            for i in range( round(sum(1 for _ in AdminList) / 50) ):
+                UserList = AdminList[i*50  :  i*50 + 50]
+                
+                await FetchStatus(UserList, Channel, "<:Admin:1257345106009264148>") # <:Admin:1257345106009264148>  <:IcyIsStupid:1257351523868999763>
+                await asyncio.sleep(18)
+        except:
+            pass
 
 ##########################################
 Client.run(Token)
